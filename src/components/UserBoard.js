3ffb5aee74d5113fe355bf9345ba77284089b906
Ronnie Goodrich <na>
2019-10-08 19:03:31 -0400

    Added Button

---
+++ src/components/UserBoard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import Block from "./Block.js"
+
+const UserBoard = (props) => {
+
+    //console.log("PROPS:" + props)
+        return (
+            <button type="text" onChange={props.changed}/>
+
+            
+        )
+    
+}
+export default UserBoard;
